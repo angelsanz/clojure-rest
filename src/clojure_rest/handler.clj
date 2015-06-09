cddbcf00bd6f62c0fc40e1011d5bbac522f8e27a
ergl <ergl@users.noreply.github.com>
2015-06-09 02:08:37 +0200

    Moved all report routes to report-routes
    Removed report routes from comments and events, added new route to handler
    Fixed a badly composed route on comment-routes

--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -10,6 +10,7 @@
             [clojure-rest.routes.auth-routes :refer [auth-routes]]
             [clojure-rest.routes.user-routes :refer [user-routes]]
             [clojure-rest.routes.event-routes :refer [event-routes]]
+            [clojure-rest.routes.report-routes :refer [report-routes]]
             [clojure-rest.routes.comment-routes :refer [comment-routes]]
             [clojure-rest.routes.coordinate-routes :refer [coordinate-routes]]))
 
@@ -37,6 +38,8 @@
            
            (context "/events" [] event-routes)
            
+           (context "/reports" [] report-routes)
+
            (context "/comments" [] comment-routes)
            
            (context "/coordinates" [] coordinate-routes)
